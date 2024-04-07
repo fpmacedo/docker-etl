49a347195ea34656880f56d2c841adfcf205a5da
fpmacedo <lipe_macedo@msn.com>
2024-04-07 14:56:08 -0300

    update and insert at raw finished

--- python-etl/etl.py
+++ python-etl/etl.py
@@ -1,45 +1,59 @@
 import csv
+import datetime
 import json
 import psycopg2
+import os
 import configparser
+from sql_queries import create_raw_temp_table, insert_raw_temp_table, upsert_raw_table
 
 config = configparser.ConfigParser()
 config.read('./etl.cfg')
-print(*config['DWH'].values())
 
 # Establish a connection to the PostgreSQL database
 conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['DWH'].values()))
 conn.autocommit = True
-cursor = conn.cursor()
+cur = conn.cursor()
 
-# Define the path to your CSV file
-csv_file_path = './20230904-130010492.csv'
+# List to store the names of CSV files
+csv_files = []
 
+# Loop through each file in the specified folder
+for file_name in os.listdir('./data'):
+    # Check if the file is a CSV by its extension
+    if file_name.endswith('.csv'):
+        csv_files.append(file_name)
+    
+    # Sort the list of CSV file names
+    csv_files_sorted = sorted(csv_files)
+    
 
 # Open the CSV file
-with open(csv_file_path, mode='r') as file:
-    reader = csv.DictReader(file)
+for csv_file_name in csv_files_sorted:
+    print(f"Start insert data: {datetime.datetime.now()}")
+    #create raw_temp table
+    cur.execute(create_raw_temp_table)
+    #conn.commit()
+
+    with open(f'data/{csv_file_name}', mode='r') as file:
+        reader = csv.DictReader(file)
 
     # Iterate over the CSV rows
-    for row in reader:
-        # Parse the JSON string in 'array_trackingEvents' column
-        tracking_events = json.loads(row['array_trackingEvents'].replace("'", '"').replace("None", 'null'))
+        for row in reader:
+            # Parse the JSON string in 'array_trackingEvents' column
+            tracking_events = json.loads(row['array_trackingEvents'].replace("'", '"').replace("None", 'null'))
+            # Execute the INSERT query with the parsed JSON and other column values
+            cur.execute(insert_raw_temp_table, (row['oid__id'],
+                                                row['Op'],
+                                                row['oid__id'],
+                                                row['createdAt'],
+                                                row['updatedAt'],
+                                                row['lastSyncTracker'],
+                                                json.dumps(tracking_events)))
+    
+    cur.execute(upsert_raw_table)
 
-        # Prepare the INSERT query. Make sure to list all columns you want to insert data into
-        insert_query = """
-        INSERT INTO raw (Op, oid__id, createdAt, updatedAt, lastSyncTracker, array_trackingEvents)
-        VALUES (%s, %s, %s, %s, %s, %s);
-        """
-        # Execute the INSERT query with the parsed JSON and other column values
-        cursor.execute(insert_query, (row['Op'],
-                                      row['oid__id'],
-                                      row['createdAt'],
-                                      row['updatedAt'],
-                                      row['lastSyncTracker'],
-                                      json.dumps(tracking_events)))
-
-print("Data inserted successfully")
+print(f"Data inserted successfully: {datetime.datetime.now()}")
 
 # Close the cursor and the connection
-cursor.close()
+cur.close()
 conn.close()
--- python-etl/sql_queries.py
+++ python-etl/sql_queries.py
@@ -23,3 +23,38 @@ create_raw_table =  """
 
 create_table_queries = [create_raw_table]
 drop_table_queries = [drop_raw_table]
+
+
+#UPSERT WITH CONFLICT WITHOUT USING UPSERT
+
+create_raw_temp_table = """
+                    DROP TABLE IF EXISTS raw_temp;
+
+                    CREATE TABLE raw_temp (
+                              Op VARCHAR(50),
+                              oid__id UUID,
+                              createdAt BIGINT,
+                              updatedAt BIGINT,
+                              lastSyncTracker BIGINT,
+                              array_trackingEvents JSONB,
+                              PRIMARY KEY (oid__id)
+                                );
+                   """
+
+insert_raw_temp_table = """
+                            DELETE FROM raw_temp
+                            WHERE raw_temp.oid__id = (%s);
+
+                            INSERT INTO raw_temp (Op, oid__id, createdAt, updatedAt, lastSyncTracker, array_trackingEvents)
+                            VALUES (%s, %s, %s, %s, %s, %s);
+                        """
+
+upsert_raw_table = """
+                        DELETE FROM raw
+                        USING raw_temp
+                        WHERE raw.oid__id = raw_temp.oid__id;
+                        
+                        INSERT INTO raw (Op, oid__id, createdAt, updatedAt, lastSyncTracker, array_trackingEvents)
+                        SELECT Op, oid__id, createdAt, updatedAt, lastSyncTracker, array_trackingEvents
+                        FROM raw_temp;
+                  """
